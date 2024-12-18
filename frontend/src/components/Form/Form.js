67c27ac3a69785a70596552d098a356842ce6f21
Tayanne <tayh.snovaes@gmail.com>
2024-12-18 20:26:31 -0300

    feat(Form): use setRecommendations to update dataRecommendations

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,13 +1,13 @@
 // Form.js
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Preferences, Features, RecommendationType } from './Fields';
 import { SubmitButton } from './SubmitButton';
 import useProducts from '../../hooks/useProducts';
 import useForm from '../../hooks/useForm';
 import useRecommendations from '../../hooks/useRecommendations';
 
-function Form() {
+function Form({ setRecommendations }) {
   const { preferences, features, products } = useProducts();
   const { formData, handleChange } = useForm({
     selectedPreferences: [],
@@ -15,15 +15,12 @@ function Form() {
     selectedRecommendationType: '',
   });
 
-  const { getRecommendations, recommendations } = useRecommendations(products);
+  const { getRecommendations } = useRecommendations(products);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const dataRecommendations = getRecommendations(formData);
-
-    /**
-     * Defina aqui a lógica para atualizar as recomendações e passar para a lista de recomendações
-     */
+    setRecommendations(dataRecommendations);
   };
 
   return (
