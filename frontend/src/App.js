f6464ecc413657ae0bf4f8f8a5c16cbacecd4322
Tayanne <tayh.snovaes@gmail.com>
2024-12-18 20:26:07 -0300

    feat(App): use setRecommendations as a params

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,26 @@ import RecommendationList from './components/RecommendationList/RecommendationLi
 function App() {
   const [recommendations, setRecommendations ] = useState([])
 
-  /**
-   * Dadas atualizações no formulário, necessário atualizar a lista de recomendações
-   */
-
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold mb-8">Recomendador de Produtos RD Station</h1>
+      <h1 className="text-3xl font-bold mb-8">
+        Recomendador de Produtos RD Station
+      </h1>
       <div className="bg-white p-8 rounded-lg shadow-md w-full md:w-3/4 lg:w-1/2 grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="col-span-2 mb-4">
           <p className="text-lg">
-            Bem-vindo ao Recomendador de Produtos RD Station. Aqui você pode encontrar uma variedade de produtos da RD Station, cada um projetado para atender às necessidades específicas do seu negócio. De CRM a Marketing, de Conversas a Inteligência Artificial, temos uma solução para ajudar você a alcançar seus objetivos. Use o formulário abaixo para selecionar suas preferências e funcionalidades desejadas e receba recomendações personalizadas de produtos que melhor atendam às suas necessidades.
+            Bem-vindo ao Recomendador de Produtos RD Station. Aqui você pode
+            encontrar uma variedade de produtos da RD Station, cada um projetado
+            para atender às necessidades específicas do seu negócio. De CRM a
+            Marketing, de Conversas a Inteligência Artificial, temos uma solução
+            para ajudar você a alcançar seus objetivos. Use o formulário abaixo
+            para selecionar suas preferências e funcionalidades desejadas e
+            receba recomendações personalizadas de produtos que melhor atendam
+            às suas necessidades.
           </p>
         </div>
         <div>
-          <Form />
+          <Form setRecommendations={setRecommendations} />
         </div>
         <div>
           <RecommendationList recommendations={recommendations} />
