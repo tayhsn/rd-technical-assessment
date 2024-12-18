12d312d3bb483edec890e33b98ef2fa1f719cd9d
Tayanne <tayh.snovaes@gmail.com>
2024-12-18 20:52:45 -0300

    feat(getRecommendations): add the logic to return the recommended products

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -1,12 +1,53 @@
 // getRecommendations.js
 
 const getRecommendations = (
-  formData = { selectedPreferences: [], selectedFeatures: [] },
+  formData = {
+    selectedPreferences: [],
+    selectedFeatures: [],
+    selectedRecommendationType: '',
+  },
   products
 ) => {
-  /**
-   * Crie aqui a lógica para retornar os produtos recomendados.
-   */
+  const { selectedFeatures, selectedPreferences, selectedRecommendationType } =
+    formData;
+
+  const scoredProducts = products
+    .map((product) => {
+      const preferenceScore = product.preferences.filter((preference) =>
+        selectedPreferences?.includes(preference)
+      ).length;
+
+      const featureScore = product.features.filter((feature) =>
+        selectedFeatures?.includes(feature)
+      ).length;
+
+      return {
+        ...product,
+        score: preferenceScore + featureScore,
+      };
+    })
+    .sort((a, b) => b.score - a.score);
+
+  const topScore = scoredProducts[0].score;
+  const tiedProducts = scoredProducts.filter(
+    (product) => product.score === topScore
+  );
+
+  switch (selectedRecommendationType) {
+    case 'SingleProduct':
+      const bestMatch = scoredProducts[0];
+      const lastMatch = tiedProducts[tiedProducts.length - 1];
+
+      if (tiedProducts) {
+        return [lastMatch];
+      }
+
+      return [bestMatch];
+
+    case 'MultipleProduct':
+    default:
+      return scoredProducts.filter((product) => product.score > 0);
+  }
 };
 
 export default { getRecommendations };
